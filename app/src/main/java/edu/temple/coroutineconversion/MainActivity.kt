ed309c043409db2f1a20468b6479a072e3ec2296
Jonathan Augustin <tun45218@temple.edu>
2024-03-27 16:48:45 -0400

    App now uses Coroutines, and still works the same.

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -2,12 +2,11 @@ package edu.temple.coroutineconversion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -23,7 +22,12 @@ class MainActivity : AppCompatActivity() {
         cakeImageView = findViewById(R.id.imageView)
 
         findViewById<Button>(R.id.revealButton).setOnClickListener {
-            CoroutineScope(Dispatchers.Main).launch()
+            CoroutineScope(Dispatchers.Main).launch {
+                repeat(100) {
+                    cakeImageView.alpha = it / 100f
+                    delay(40)
+                }
+            }
         }
     }
 }
